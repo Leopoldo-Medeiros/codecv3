4420a2bc4495d1e0c66a442d1148c59648e0401d
Rodrigo Tschope <r_tschope@apple.com>
2023-03-21 19:25:40 +0000

    few bugs fixes

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -337,3 +337,16 @@
     </div>
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  title: 'CODE CV - Princing',
+  meta: [
+    { name: 'description',
+      content: 'CODE CV is a consulting company focused on the career of IT professionals.\n' +
+        'The key focus is to support the preparation of a professional CV, building a LinkedIn profile, and\n' +
+        'training for the interview processes.\n' +
+        'Our team is made up of IT professionals with experience in the market.' }
+  ],
+})
+
+</script>
